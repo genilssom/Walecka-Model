cab42af187afa0098d417a90ff930f344e3b243e
Genilson <genilsonc01@gmail.com>
2024-07-08 18:20:27 -0300

    added new files

---
+++ functions/constant_couplings.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from scipy.optimize import fsolve
+from .constants import *
+
+
+
+
+
---
+++ functions/constants.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+hbc = 197.326967883
+pi = np.pi
+m_N = 939./hbc                 # Nucleon mass in fm^-1
+m_omega = 783./hbc             # Omega meson mass in fm^-1
+mw2 = m_omega**2
+m_sigma = 550./hbc             # Sigma meson mass in fm^-1
+ms2 = m_sigma**2
+nb_sat = 0.153
+
